132e0e51da6fb1bb017f3fcb25fa018d1d6106e4
ethanrice <35052125+ethanrice@users.noreply.github.com>
2018-07-19 22:54:29 -0400

    Add files via upload

---
+++ bamazonCustomer.js
@@ -0,0 +1,55 @@
+
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "Bamazon"
+})
+
+function start(){
+
+connection.query('SELECT * FROM Products', function(err, res){
+  if(err) throw err;
+
+  console.log('Bamazon')
+  console.log('----------------------------------------------------------------------------------------------------')
+
+  for(var i = 0; i<res.length;i++){
+    console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+    console.log('--------------------------------------------------------------------------------------------------')
+  }
+
+  console.log(' ');
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "Type Product ID",
+      validate: function(value){
+        if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+          return true;
+        } else{
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "qty",
+      message: "How many would you like?",
+      validate: function(value){
+        if(isNaN(value)){
+          return false;
+        } else{
+          return true;
+        }
+      }
+    }
+    ]).then(function(ans){
+      var whichProduct = (ans.id)-1;
+      var howMany = parseInt(ans.qty);
+      var total = parseFloat(((res[whichProduct].Price)*howMany).toFixed(2));
